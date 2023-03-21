e768d5d2e17216db36a87972c36515561169dd30
putridmayo <kimmikun@gmail.com>
2023-03-21 20:49:09 +1100

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
+import { firebaseConfig } from './config/Config'
+import { initializeApp, intializeApp} from 'firebase/app'
 import './App.css';
 
+
+const FirebaseApp = initializeApp ( firebaseConfig )
 function App() {
   return (
     <div className="App">
+      <button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+ 
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Thanks Johannes
         </p>
         <a
           className="App-link"
---
+++ src/config/Config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD5T9G4URXmUU0vDPTM2AlW0EV3Gr-mXOk",
+    authDomain: "reacttafe.firebaseapp.com",
+    projectId: "reacttafe",
+    storageBucket: "reacttafe.appspot.com",
+    messagingSenderId: "792786628077",
+    appId: "1:792786628077:web:cf68e0deb031cd4b2f0be4"
+  };
