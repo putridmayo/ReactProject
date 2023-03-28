f3ca2e5fd8bb4f9a4dbf8246a52a6b2a1ad658bb
putridmayo <kimmikun@gmail.com>
2023-03-28 21:29:59 +1100

    Update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import { Test } from './components/Test';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
+import { Contact } from './pages/Contact';
 import { Routes, Route } from 'react-router-dom';
 
 const FirebaseApp = initializeApp ( firebaseConfig )
@@ -12,8 +13,10 @@ function App() {
     return (
       <div className="App">
        <Routes>
-        <Route path="/" element={ <Home/>} />
+       <Route path="/" element={ <Home/>} />
+        <Route path="/home" element={ <Home/>} />
         <Route path="/about" element={ <About/>} />
+        <Route path="/contact" element={ <Contact/>} />
        </Routes>
       </div>
     );
